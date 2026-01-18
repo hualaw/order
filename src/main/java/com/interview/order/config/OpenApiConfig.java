34a0cfac90bec49fd536b01341c272baeb3e0266
Hua <hualaw@gmail.com>
2026-01-18 20:10:29 +0800

    configure JWT in swagger-ui

--- src/main/java/com/interview/order/config/OpenApiConfig.java
+++ src/main/java/com/interview/order/config/OpenApiConfig.java
@@ -1,16 +1,27 @@
 package com.interview.order.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .info(new Info()
                         .title("Order Service API")
                         .version("v0.0.1")
